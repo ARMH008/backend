45987ce8be7d979362788879b3caa7bfbaecca62
ARMH008 <158465673+ARMH008@users.noreply.github.com>
2024-08-06 14:08:29 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://pruthvij-rrfq.vercel.app/",
     credentials: true,
     origin: true,
     optionsSuccessStatus: 200,
